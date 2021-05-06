66b1049003c73ffb3c7bc388164d242c377ed9f6
marcello <mcfanda@gmail.com>
2021-05-06 20:41:51 +0200

    ! cleaning

--- local/install.R
+++
@@ -1,9 +0,0 @@
-install.packages('jmvtools', repos=c('https://repo.jamovi.org', 'https://cran.r-project.org'))
-
-jmvtools::version()
-jmvtools::check()
-
-source("local/functions.R")
-installme("pathj")
-
-
